f94a1a9088f97bc5d3ea5aea9335144ad19c80b6
= <=>
2024-12-31 21:32:12 +0200

    change header on scroll

--- tutor-website/js/script.js
+++ tutor-website/js/script.js
@@ -1,5 +1,15 @@
 const burger = document.querySelector(".burger");
 const list = document.querySelector(".main-menu-list");
+const nav = document.querySelector(".navbar");
 burger.addEventListener("click", function () {
   list.classList.toggle("active");
+});
+document.addEventListener("scroll", (e) => {
+  if (window.scrollY >= 100) {
+    if (!nav.classList.contains("active")) {
+      nav.classList.add("active");
+    }
+  } else {
+    nav.classList.remove("active");
+  }
 });
